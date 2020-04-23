e2eaf7b5829b0f05c8a055ade6e29b7a0d2c0335
Wyatt Pultz <wpultz@gmail.com>
2020-04-23 13:05:42 -0400

    Use redux-thunk instead of custom hook for api requests (#3)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 
 import hnSearch from './hnSearch/hnSearch'
 
 // TODO figure out the TS error with the reducer signature. Seems like maybe related to not using combineReducers
 // @ts-ignore
-export const store = createStore(hnSearch, composeWithDevTools())
+export const store = createStore(hnSearch, composeWithDevTools(applyMiddleware(thunk)))
